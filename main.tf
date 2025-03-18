6adb29024213308e9b938efaa40c617dcd9931a6
Vijayanagendrakadimi <nagendrachowdary397@gmail.com>
2025-03-18 10:30:33 +0530

    third commit

--- main.tf
+++ main.tf
@@ -1,4 +1,16 @@
-provider "docker" {}
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0.2"
+    }
+  }
+}
+
+provider "docker" {
+  host = "unix:///var/run/docker.sock" # For Linux/macOS
+  # host = "npipe:////./pipe/docker_engine" # For Windows
+}
 
 resource "docker_image" "html_image" {
   name         = "html-server"
